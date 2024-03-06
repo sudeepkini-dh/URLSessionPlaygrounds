39346203e873dce831587d2beaadd37f1c05edb2
theScud <sudeepkini@gmail.com>
2024-03-06 10:37:35 +0100

    Remove Arena playgrounds

--- Arena-Playground/Arena-Playground.playgroundbook/Contents/Chapters/Chapter1.playgroundchapter/Pages/My Playground.playgroundpage/main.swift
+++
@@ -1,3 +0,0 @@
-// ℹ️ The source files of your dependencies have been copied into the
-//    UserModule/Sources folder and their public interfaces are
-//    available without requiring a module import.
